e4a2e14d7bc87825787488e838fdc27af1be069a
Siddhesh <siddhehsongire@gmail.com>
2022-11-12 00:29:47 +0530

    Setting up environment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Welcome, Siddhesh</h1>
+			<h4>Your tasks</h4>
+		</div>
+	);
 }
 
 export default App;
