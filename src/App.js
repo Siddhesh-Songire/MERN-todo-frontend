95b46c99ceaaa26d044cd43644742dbde632d91b
Siddhesh <siddhehsongire@gmail.com>
2022-11-12 00:42:07 +0530

    Added complete css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ function App() {
 		<div className="App">
 			<h1>Welcome, Siddhesh</h1>
 			<h4>Your tasks</h4>
+
+			<div className="todos">
+				<div className="todo">
+					<div className="checkbox"></div>
+
+					<div className="text">Get the bread</div>
+
+					<div className="delete-todo">x</div>
+				</div>
+
+				<div className="todo">
+					<div className="checkbox"></div>
+
+					<div className="text">Order food</div>
+
+					<div className="delete-todo">x</div>
+				</div>
+
+				<div className="todo is-complete">
+					<div className="checkbox"></div>
+
+					<div className="text">Go to gym</div>
+
+					<div className="delete-todo">x</div>
+				</div>
+
+			</div>
 		</div>
 	);
 }
